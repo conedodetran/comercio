3c0c3dc49654d28b7e1ed64f0bf03f2d177eb575
Arthur Miguel <arthurmiguelmartin@gmail.com>
2023-06-06 09:15:15 -0300

    Tela de Produtos e model produtos parcial

--- src/main/java/com/itb/inf2dm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2dm/comercio/model/Produto.java
@@ -1,5 +1,14 @@
 package com.itb.inf2dm.comercio.model;
 
 public class Produto {
-
+	
+	private Long id;
+	private String descricao;
+	private String nome;
+	private String codigoBarras;
+	private double preco;
+	
+	// public : Modificador de acesso livre para todas as classes.
+	// private : Modificador de acesso restrito apenas para membros da
+	// da própria classe. Entende-se por "membros" (atributos e/ou métodos)
 }
