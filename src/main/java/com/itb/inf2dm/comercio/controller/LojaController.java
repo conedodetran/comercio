ef712f4316a18b7711f2f5d61faa9ca183a1e56d
Arthur Miguel <arthurmiguelmartin@gmail.com>
2023-06-13 09:18:34 -0300

    Aula 13/06 Parte final

--- src/main/java/com/itb/inf2dm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2dm/comercio/controller/LojaController.java
@@ -9,13 +9,19 @@ import com.itb.inf2dm.comercio.model.Produto;
 @Controller
 @RequestMapping("/comercio/produtos")
 public class LojaController {
+	
+	List<Produto> listaDeProdutos = new ArrayList<Produto>();
+	
 	@GetMapping("/listar")
 	public String listarProdutos() {
 		
 		Produto p1 = new Produto();
-		p1.id = 20l;
+		p1.setId(201);
+		p1.setNome("Máquina de Lavar Brastemp 12 Litros");
+		p1.setCodigoBarras("DWA89E7Q89WYHDAWI");
+		p1.setPreco(3654.12);
 				
-				
+		listaDeProdutos.add(p1);
 		return "produtos";
 				
 	}
--- src/main/java/com/itb/inf2dm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2dm/comercio/model/Produto.java
@@ -14,7 +14,34 @@ public class Produto {
 
 	// Métodos Setter's e Getter's : Adicionar e Recuperar dados do atributo "RESPECTIVAMENTE"
 	
+	public Long getId() {
+		return id;
+	}
 	public void setId(Long id) {
 		this.id = id;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCodigoBarras() {
+		return codigoBarras;
+	}
+	public void setCodigoBarras(String codigoBarras) {
+		this.codigoBarras = codigoBarras;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
 	}
 }
