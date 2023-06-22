0802ea9683591c4995aa50bc02e866cca50a8158
Arthur Miguel <arthurmiguelmartin@gmail.com>
2023-06-22 08:28:47 -0300

    Aula LIP1 22/06

--- src/main/java/com/itb/inf2dm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2dm/comercio/controller/LojaController.java
@@ -1,5 +1,8 @@
 package com.itb.inf2dm.comercio.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,12 +19,14 @@ public class LojaController {
 	public String listarProdutos() {
 		
 		Produto p1 = new Produto();
-		p1.setId(201);
+		p1.setId(20l);
 		p1.setNome("Máquina de Lavar Brastemp 12 Litros");
 		p1.setCodigoBarras("DWA89E7Q89WYHDAWI");
 		p1.setPreco(3654.12);
 				
 		listaDeProdutos.add(p1);
+		
+		model.addAttribute("listadeprodutos", listaDeProdutos);
 		return "produtos";
 				
 	}
