4e5c6a376c0c0046e79022ef207152442953e8fa
Arthur Miguel <arthurmiguelmartin@gmail.com>
2023-06-29 08:31:19 -0300

    Aula do dia 29/06

--- src/main/java/com/itb/inf2dm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2dm/comercio/controller/LojaController.java
@@ -1,7 +1,7 @@
 package com.itb.inf2dm.comercio.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,37 +13,37 @@ import com.itb.inf2dm.comercio.model.Produto;
 @Controller
 @RequestMapping("/comercio/produtos")
 public class LojaController {
-	
 	List<Produto> listaDeProdutos = new ArrayList<Produto>();
 	
 	@GetMapping("/listar")
-	public String listarProdutos(Model mordel) {
+	public String listarProdutos(Model model) {
+		
 		
 		Produto p1 = new Produto();
-		p1.setId(20l);
-		p1.setNome("Máquina de Lavar Brastemp 12 Litros");
-		p1.setCodigoBarras("DWA89E7Q89WYHDAWI");
+		p1.setId = (20l);
+		p1.setNome("rtx 3060");
+		p1.setCodigoBarras("JHDYEJOUSNEU5411");
 		p1.setPreco(3654.12);
-		p1.setDescricao(null);
-		p1.setCodStatus(true);
-		
-		Produto p2 = new Produto();
-		p2.setId(20l);
-		p2.setNome("Celular");
-		p2.setCodigoBarras("DSAHDUAJSAUIDSJNDSJA");
-		p2.setPreco(1890.99);
-		p2.setDescricao(null);
-		p2.setCodStatus(true);
-		
+		p1.setDescricao("COMPUTADOR TERABYTE");
+		p1.setStatus(true);
 		
 		listaDeProdutos.add(p1);
-		listaDeProdutos.add(p2);
 		
-		model.addAttribute("listaDeProdutos", listaDeProdutos);
-		return "produtos";
-				
-	}
+		
+		Produto p2 = new Produto();
+		p2.setId = (21l);
+		p2.setNome("rx 6750 xt");
+		p2.setCodigoBarras("JHDYEJOUSNEU25411");
+		p2.setPreco(3654.12);
+		p2.setDescricao("COMPUTADOR PICHAU GAMER");
+		p2.setStatus(false);
+		
+	    listaDeProdutos.add(p2);
 	
+    model.addAttribute("listaDeProdutos", listaDeProdutos);
+	
+	return "produtos";
+	}
 	@GetMapping("/novo-prod")
 	public String novoProduto() {
 		return "novo-prod";
--- src/main/java/com/itb/inf2dm/comercio/model/Produto.java
+++ src/main/java/com/itb/inf2dm/comercio/model/Produto.java
@@ -1,51 +1,66 @@
 package com.itb.inf2dm.comercio.model;
 
 public class Produto {
-	
-	private Long id;
+	private long id;
 	private String descricao;
 	private String nome;
 	private String codigoBarras;
 	private double preco;
+	private boolean status;
+	public long setId;
 	
-	// public : Modificador de acesso livre para todas as classes.
-	// private : Modificador de acesso restrito apenas para membros da
-	// da própria classe. Entende-se por "membros" (atributos e/ou métodos)
-
-	// Métodos Setter's e Getter's : Adicionar e Recuperar dados do atributo "RESPECTIVAMENTE"
+	//public: modificador de acesso livre para todas as classes
+	//private: Modificador de acesso restrito apenas membros 
+	//da própia classe, Entende-se por "membros" (atributos e/ou metodos).
 	
-	public Long getId() {
-		return id;
-	}
+	//Métodos setter's e Getter's: Adicionar e recuperar dados do atributo "RESPECTIVAMENTE"
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getDescricao() {
-		return descricao;
+	public Long getId() {
+		return id;
 	}
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+
+    }
+	public String getDescricao() {
+		return descricao;
 	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+		
+	}
+	
 	public String getNome() {
 		return nome;
 	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getCodigoBarras() {
-		return codigoBarras;
-	}
+	
 	public void setCodigoBarras(String codigoBarras) {
 		this.codigoBarras = codigoBarras;
 	}
-	public double getPreco() {
-		return preco;
+	
+	public String getCodigoBarras() {
+		return codigoBarras;
 	}
+	
 	public void setPreco(double preco) {
 		this.preco = preco;
-	}
-	public void setCodStatus(boolean b) {
-		// TODO Auto-generated method stub
 		
 	}
+	 public double getPreco() {
+		 return preco;
+	 }
+	 
+	 public void setStatus(Boolean status) {
+		 this.status = status;
+	 }
+	 
+	 public Boolean getStatus() {
+		return status;
+		 
+	 }
+
 }
