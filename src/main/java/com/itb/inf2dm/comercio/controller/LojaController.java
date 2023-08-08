448016f4fd356d5978b0f344702cdd94432787e9
Arthur Miguel <arthurmiguelmartin@gmail.com>
2023-08-08 12:26:14 -0300

    Aula dia 08/08/23

--- src/main/java/com/itb/inf2dm/comercio/controller/LojaController.java
+++ src/main/java/com/itb/inf2dm/comercio/controller/LojaController.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.itb.inf2dm.comercio.model.Produto;
@@ -18,7 +19,7 @@ public class LojaController {
 	@GetMapping("/listar")
 	public String listarProdutos(Model model) {
 		
-		
+		/*
 		Produto p1 = new Produto();
 		p1.setId = (20l);
 		p1.setNome("RTX 3060 TI 16GB");
@@ -39,13 +40,22 @@ public class LojaController {
 		p2.setStatus(false);
 		
 	    listaDeProdutos.add(p2);
-	
+		
+		*/
     model.addAttribute("listaDeProdutos", listaDeProdutos);
 	
 	return "produtos";
 	}
+	
 	@GetMapping("/novo-prod")
-	public String novoProduto() {
+	public String novoProduto(Model model, Produto produto) {
+		model.addAttribute("produto", produto);
 		return "novo-prod";
+	}
+	
+	@PostMapping("/add-prod")
+	public String gravarNovoProduto(Model model, Produto produto) {
+	listaDeProdutos.add(produto);
+	return "redirect:/comercio/produtos/listar";
 	}
 }
